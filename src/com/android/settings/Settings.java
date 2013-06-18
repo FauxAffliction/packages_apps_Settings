019285cb5a90ed4468d86fbc3be15acae0f4c8c2
FauxAffliction <aaron.m.wright91@gmail.com>
2013-06-18 02:21:47 -0500

    Slim Halo Fixes
    Change-Id: Iaf030bdb9fd741500970896a02882e05b4f3f4a3

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -883,5 +883,6 @@ public class Settings extends PreferenceActivity
     public static class ASSRamBarActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
     public static class NotificationShortcutsSettingsActivity extends Settings { /* empty */ }
+    public static class HaloActivity extends Settings { /* empty */ }
     public static class PowerMenuSettingsActivity extends Settings { /* empty */ }
 }
