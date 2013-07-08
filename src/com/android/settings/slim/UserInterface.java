5f41886bdf8fea6d3d5057c8b9fb6d4015baff0b
FauxAffliction <aaron.m.wright91@gmail.com>
2013-07-08 04:03:53 -0500

    Cleanup/Remove some stuff merged over on accident && add Recents Ram Circle
    Change-Id: I6f60c330ea57df358ba23021d9aa118b07ac5eb0

--- src/com/android/settings/slim/UserInterface.java
+++ src/com/android/settings/slim/UserInterface.java
@@ -19,6 +19,8 @@ package com.android.settings.slim;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.app.ActivityManager;
@@ -32,12 +34,14 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.text.Spannable;
 
 import com.android.settings.R;
+import com.android.settings.util.Helpers;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 import android.widget.EditText;
@@ -49,6 +53,7 @@ public class UserInterface extends SettingsPreferenceFragment implements OnPrefe
     private static final String MISC_SETTINGS = "misc";
     private static final String PREF_USE_ALT_RESOLVER = "use_alt_resolver";
     private static final String KEY_RECENTS_RAM_BAR = "recents_ram_bar";
+    private static final String KEY_RECENTS_RAM_CIRCLE = "recents_ram_circle";
     private static final String KEY_CLEAR_RECENTS_POSITION = "clear_recents_position";
     private static final String KEY_DUAL_PANE = "dual_pane";
     private static final String KEY_HIGH_END_GFX = "high_end_gfx";
@@ -59,6 +64,7 @@ public class UserInterface extends SettingsPreferenceFragment implements OnPrefe
     private CheckBoxPreference mUseAltResolver;
     private PreferenceCategory mMisc;
     private Preference mRamBar;
+    CheckBoxPreference mRamCircle;
     ListPreference mClearPosition;
     private CheckBoxPreference mDualPane;
     private CheckBoxPreference mHighEndGfx;
@@ -77,6 +83,7 @@ public class UserInterface extends SettingsPreferenceFragment implements OnPrefe
         addPreferencesFromResource(R.xml.user_interface_settings);
 
         PreferenceScreen prefs = getPreferenceScreen();
+	ContentResolver cr = mContext.getContentResolver();
 
         mMisc = (PreferenceCategory) prefs.findPreference(MISC_SETTINGS);
 
@@ -104,6 +111,10 @@ public class UserInterface extends SettingsPreferenceFragment implements OnPrefe
         mRamBar = findPreference(KEY_RECENTS_RAM_BAR);
         mRamBar.setOnPreferenceChangeListener(this);
         updateRamBar();
+
+	mRamCircle = (CheckBoxPreference) findPreference(KEY_RECENTS_RAM_CIRCLE);
+        mRamCircle.setChecked(Settings.System.getBoolean(mContext.getContentResolver(),
+                Settings.System.RECENTS_RAM_CIRCLE, false));
 
 	mClearPosition = (ListPreference) findPreference(KEY_CLEAR_RECENTS_POSITION);
         int ClearSide = Settings.System.getInt(getActivity().getContentResolver(),
@@ -193,6 +204,11 @@ public class UserInterface extends SettingsPreferenceFragment implements OnPrefe
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.ACTIVITY_RESOLVER_USE_ALT,
                     (Boolean) newValue ? 1 : 0);
+            return true;
+	} else if (preference == mRamCircle) {
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_RAM_CIRCLE,
+                    ((CheckBoxPreference) preference).isChecked());
             return true;
         } else if (preference == mDualPane) {
             Settings.System.putInt(getActivity().getContentResolver(),
